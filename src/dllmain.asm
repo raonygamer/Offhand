6309b1d36f3b44a1de4b2ed540228a00aa3ac15c
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-04-06 00:06:38 +0100

    FULLY WORKING DECOMP

--- src/dllmain.asm
+++ src/dllmain.asm
@@ -1,7 +1,13 @@
 section .text
 extern ItemUseInventoryTransaction_ctor
+extern NetworkItemStackDescriptor_ctor
 
 global ??0ItemUseInventoryTransaction@@QEAA@XZ:
 ??0ItemUseInventoryTransaction@@QEAA@XZ:
     mov rax, [rel ItemUseInventoryTransaction_ctor]
+    jmp rax
+
+global ??0NetworkItemStackDescriptor@@QEAA@AEBVItemStack@@@Z:
+??0NetworkItemStackDescriptor@@QEAA@AEBVItemStack@@@Z:
+    mov rax, [rel NetworkItemStackDescriptor_ctor]
     jmp rax
--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -17,8 +17,11 @@
 #include <minecraft/src/common/world/gamemode/GameModeMessenger.hpp>
 #include <minecraft/src/common/world/inventory/network/ItemStackNetManagerBase.hpp>
 #include <minecraft/src/common/world/events/PlayerEventListener.hpp>
+#include <minecraft/src/common/world/inventory/transaction/ComplexInventoryTransaction.hpp>
+#include <minecraft/src/common/world/inventory/transaction/InventoryTransactionManager.hpp>
 
 extern "C" void* ItemUseInventoryTransaction_ctor = nullptr;
+extern "C" void* NetworkItemStackDescriptor_ctor = nullptr;
 
 void OnRegisterItems(RegisterItemsEvent& event)
 {
@@ -26,37 +29,6 @@ void OnRegisterItems(RegisterItemsEvent& event)
 		//Log::Info("Item ID: {:s}", pair.first.getString());
         pair.second.get()->setAllowOffhand(true);
     }
-}
-
-//class IGameModeTimer;
-//class IGameModeMessenger;
-//class SurvivalMode;
-
-//enum class InventoryTransactionError : uint32_t
-//{
-//    Unknown = 0x0,
-//    NoError = 0x1,
-//    BalanceMismatch = 0x2,
-//    SourceItemMismatch = 0x3,
-//    InventoryMismatch = 0x4,
-//    SizeMismatch = 0x5,
-//    AuthorityMismatch = 0x6,
-//    StateMismatch = 0x7,
-//    ApiDenied = 0x8,
-//};
-
-
-
-
-
-SafetyHookInline _GameMode_buildBlock;
-
-void Test(Container& container, int unkn, const ItemStack& item, const ItemStack& item2) {
-    Log::Info("Test: {} {} {}", item.mItem->getRawNameId(), item2.mItem->getRawNameId(), unkn);
-}
-
-void Test2() {
-	Log::Info("Test2"); 
 }
 
 class PlayerEventCoordinator {
@@ -68,6 +40,7 @@ public:
     }
 };
 
+SafetyHookInline _GameMode_buildBlock;
 
 bool GameMode_buildBlock(GameMode* self, BlockPos* pos, FacingID face, bool isSimTick) {
     Player& player = self->mPlayer;
@@ -87,27 +60,31 @@ bool GameMode_buildBlock(GameMode* self, BlockPos* pos, FacingID face, bool isSi
         || !isSimTick
         || item->canUseOnSimTick();
 
-    if (!mayUseItem) return false;
+    if (!mayUseItem) {
+        Log::Info("May not use item!!");
+        return false;
+    }
 
     self->mPlayerLastPosition = *player.getPosition();
     std::unique_ptr<ItemUseInventoryTransaction> transaction = std::make_unique<ItemUseInventoryTransaction>();
-    Log::Info("transaction type {}", (int)transaction->mType);
-    std::optional<gsl::final_action<std::function<void()>>> onTransactionEnd;
 
-    if (player.isClientSide() && player.mItemStackNetManager != nullptr) {
-        onTransactionEnd.emplace(player.mItemStackNetManager->_tryBeginClientLegacyTransactionRequest());
-    }
-    else {
-        onTransactionEnd.emplace([] {
-            Log::Info("Todo implement this branch");
-        });
-    }
+	bool useLegacyTransaction = player.isClientSide() && player.mItemStackNetManager != nullptr;
+
+    // gsl::final_action runs code at the end of the scope
+    // Game only uses for legacy transaction
+    auto onScopeClosed = useLegacyTransaction
+        ? player.mItemStackNetManager->_tryBeginClientLegacyTransactionRequest()
+        : gsl::final_action<std::function<void()>>([]() {});
 
     InteractionResult result;
 
     playerInv->createTransactionContext(
-        [](Container&, int, const ItemStack&, const ItemStack) {
-            Log::Info("callback called! todo impl");
+        [&transaction, &self](Container& container, unsigned int slot, const ItemStack& oldStack, const ItemStack newStack) {
+            InventorySource source(InventorySourceType::ContainerInventory, ContainerID::CONTAINER_ID_INVENTORY, InventorySource::InventorySourceFlags::NoFlag);
+            InventoryAction action(source, slot, oldStack, newStack);
+            
+			self->mPlayer.mTransactionManager.addExpectedAction(action);
+			transaction->mTransaction.addAction(action);
         }, 
         [&transaction, &self, pos, face, &result]() {
             Player& player = self->mPlayer;
@@ -115,7 +92,9 @@ bool GameMode_buildBlock(GameMode* self, BlockPos* pos, FacingID face, bool isSi
 
 			ItemStack selectedItemCopy = player.getSelectedItem();
             ItemInstance itemInstance(selectedItemCopy);
-            // NetworkItemStackDescriptor networkDescriptor(selectedItemCopy);
+
+            NetworkItemStackDescriptor networkDescriptor(selectedItemCopy);
+            transaction->mItem = networkDescriptor;
             
 			HitResult hitResult = level->getHitResult();
 			HitResult liquidHit = level->getLiquidHitResult();
@@ -126,22 +105,18 @@ bool GameMode_buildBlock(GameMode* self, BlockPos* pos, FacingID face, bool isSi
             FacingID faceDir = face;
             BlockPos calculatedPlacePos = self->_calculatePlacePos(selectedItemCopy, *pos, faceDir);
 
-			// From the dissasembly, this will always run... because v83 and v74 are always false. sus
-            bool v83 = false;
-            bool v74 = false;
-            if (!v83 || !v74 || self->mBuildContext.mHasBuildDirection) {
+            if (hitResult.mType == HitResultType::TILE || liquidHit.mLiquid.y == 0 || self->mBuildContext.mHasBuildDirection) {
                 transaction->mPos = *pos;
 				transaction->setTargetBlock(block);
                 transaction->mFace = faceDir;
 
                 Vec3 clickPos;
 
-                // I have no idea what this logic is is, IDA probably doesn't know what v83 is, and neither do I.
-                if ((v83 - 2) <= 1) {
+                if (hitResult.mType == HitResultType::NO_HIT) {
                     clickPos = Vec3::ZERO;
                 }
                 else {
-                    clickPos = Vec3::ZERO; // THIS IS TEMP WRONG!!!!
+                    clickPos = hitResult.mPos - Vec3(*pos);
                 }
                 
 				transaction->mClickPos = clickPos;
@@ -155,7 +130,7 @@ bool GameMode_buildBlock(GameMode* self, BlockPos* pos, FacingID face, bool isSi
                 else {
                     // IMPORTANT WILL BREAK: GameMode::_sendUseItemOnEvents uses Player::getSelectedItem oof.
                     // IDA detects it doesn't actually use it tho?
-                    result = self->useItemOn(selectedItemCopy, *pos, face, clickPos, nullptr);
+                    result = self->useItemOn(selectedItemCopy, *pos, face, hitResult.mPos, nullptr);
                 }
             }
 
@@ -189,7 +164,6 @@ bool GameMode_buildBlock(GameMode* self, BlockPos* pos, FacingID face, bool isSi
 
             std::function<EventResult(PlayerEventListener&)> ev = [&player, &itemInstance](PlayerEventListener& listener) {
                 EventResult result = listener.onPlayerItemUseInteraction(player, itemInstance);
-				Log::Info("onPlayerItemUseInteraction {}", (int)result);
                 return result;
             };
 
@@ -197,15 +171,12 @@ bool GameMode_buildBlock(GameMode* self, BlockPos* pos, FacingID face, bool isSi
         }
     );
 
-    Log::Info("Result {}", (int)result.mResult);
+    Log::Info("InteractResult: {}", result.mResult);
 
-    // info: the execute lambda of createTransactionContext is called before this function
-    // crashes if there are no transactions?
     if (player.getLevel()->isClientSide()) {
-        Log::Info("SEND!!");
-		//player.sendInventoryTransaction(transaction->mTransaction);
-    }
-
+		player.sendComplexInventoryTransaction(std::move(transaction));
+    } 
+     
     return true;
 }
 
@@ -224,9 +195,13 @@ ModFunction void Initialize(AmethystContext& ctx)
     Log::Info("PlayerInv offset: {}", offsetof(Player, playerInventory));
 
     ItemUseInventoryTransaction_ctor = (void*)SigScan("48 89 5C 24 ? 57 48 83 EC ? 48 8D 59 ? C7 41 ? ? ? ? ? 48 8D 05 ? ? ? ? 48 89 5C 24 ? 48 89 01 48 8B F9 48 8B CB E8 ? ? ? ? 33 C9 48 8D 05 ? ? ? ? 48 89 4B ? 0F 57 C0 48 89 4B ? 48 89 4B ? 48 8B 5C 24 ? 48 89 07 48 8D 05 ? ? ? ? 48 89 4F ? 48 89 4F");
+    NetworkItemStackDescriptor_ctor = (void*)SigScan("48 89 5C 24 ? 55 56 57 41 56 41 57 48 83 EC ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 44 24 ? 48 8B FA 48 8B F1 48 89 4C 24 ? 0F B6 5A");
 
     hooks.RegisterFunction<&GameMode_buildBlock>("48 89 5C 24 ? 48 89 74 24 ? 55 57 41 54 41 56 41 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 41 0F B6 F1");
 	hooks.CreateHook<&GameMode_buildBlock>(_GameMode_buildBlock, &GameMode_buildBlock);
 
-    
+    // found a global which is in some item transaction logging code, false by default
+    // enabling seems to have no change
+    //bool* enableLogger = (bool*)SlideAddress(0x597A9B5);
+	//*enableLogger = true;
 }
