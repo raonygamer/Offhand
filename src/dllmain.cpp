1cabd2833143bca1f7776fb2d393360909e717bd
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-08-17 14:17:23 +0100

    wip

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -25,6 +25,8 @@ extern "C" void* NetworkItemStackDescriptor_ctor = nullptr;
 
 void OnRegisterItems(RegisterItemsEvent& event)
 {
+    Log::Info("OnRegisterItems");
+
     for (auto& pair : event.itemRegistry.mNameToItemMap) {
 		//Log::Info("Item ID: {:s}", pair.first.getString());
         pair.second.get()->setAllowOffhand(true);
@@ -42,72 +44,194 @@ public:
 
 SafetyHookInline _GameMode_buildBlock;
 
-bool GameMode_buildBlock(GameMode* self, BlockPos* pos, FacingID face, bool isSimTick) {
-    Player& player = self->mPlayer;
-	ILevel* level = player.mLevel;
-    PlayerInventory* playerInv = player.playerInventory;
-	Inventory* inv = playerInv->mInventory.get();
+//bool GameMode_buildBlock(GameMode* self, BlockPos* pos, FacingID face, bool isSimTick) {
+//    Player& player = self->mPlayer;
+//	ILevel* level = player.mLevel;
+//    PlayerInventory* playerInv = player.playerInventory;
+//	Inventory* inv = playerInv->mInventory.get();
+//
+//    const ItemStack& selectedItem = inv->getItem(playerInv->mSelected);
+//	const Item* item = selectedItem.getItem();
+//
+//    // The game seems to check things multiple times, i.e. selectedItem.isNull() covers some of the other cases in here
+//    // Going for accuracy so do anyway ig
+//    bool mayUseItem = !selectedItem.mValid
+//        || !item
+//        || selectedItem.isNull()
+//        || selectedItem.mCount == 0
+//        || !isSimTick
+//        || item->canUseOnSimTick();
+//
+//    if (!mayUseItem) {
+//        Log::Info("May not use item!!");
+//        return false;
+//    }
+//
+//    self->mPlayerLastPosition = *player.getPosition();
+//    std::unique_ptr<ItemUseInventoryTransaction> transaction = std::make_unique<ItemUseInventoryTransaction>();
+//
+//	bool useLegacyTransaction = player.isClientSide() && player.mItemStackNetManager != nullptr;
+//
+//    // gsl::final_action runs code at the end of the scope
+//    // Game only uses for legacy transaction
+//    auto onScopeClosed = useLegacyTransaction
+//        ? player.mItemStackNetManager->_tryBeginClientLegacyTransactionRequest()
+//        : gsl::final_action<std::function<void()>>([]() {});
+//
+//    InteractionResult result;
+//
+//    playerInv->createTransactionContext(
+//        [&transaction, &self](Container& container, unsigned int slot, const ItemStack& oldStack, const ItemStack newStack) {
+//            InventorySource source(InventorySourceType::ContainerInventory, ContainerID::CONTAINER_ID_INVENTORY, InventorySource::InventorySourceFlags::NoFlag);
+//            InventoryAction action(source, slot, oldStack, newStack);
+//            
+//			self->mPlayer.mTransactionManager.addExpectedAction(action);
+//			transaction->mTransaction.addAction(action);
+//        }, 
+//        [&transaction, &self, pos, face, &result]() {
+//            Player& player = self->mPlayer;
+//            ILevel* level = player.mLevel;
+//
+//			ItemStack selectedItemCopy = player.getSelectedItem();
+//            ItemInstance itemInstance(selectedItemCopy);
+//
+//            NetworkItemStackDescriptor networkDescriptor(selectedItemCopy);
+//            transaction->mItem = networkDescriptor;
+//            
+//			HitResult hitResult = level->getHitResult();
+//			HitResult liquidHit = level->getLiquidHitResult();
+//
+//			const BlockSource& region = player.getDimensionBlockSourceConst();
+//            const Block& block = region.getBlock(*pos);
+//
+//            FacingID faceDir = face;
+//            BlockPos calculatedPlacePos = self->_calculatePlacePos(selectedItemCopy, *pos, faceDir);
+//
+//            if (hitResult.mType == HitResultType::TILE || liquidHit.mLiquid.y == 0 || self->mBuildContext.mHasBuildDirection) {
+//                transaction->mPos = *pos;
+//				transaction->setTargetBlock(block);
+//                transaction->mFace = faceDir;
+//
+//                Vec3 clickPos;
+//
+//                if (hitResult.mType == HitResultType::NO_HIT) {
+//                    clickPos = Vec3::ZERO;
+//                }
+//                else {
+//                    clickPos = hitResult.mPos - Vec3(*pos);
+//                }
+//                
+//				transaction->mClickPos = clickPos;
+//                transaction->mFromPos = *player.getPosition();
+//                transaction->mActionType = face == FacingID::MAX ? ItemUseInventoryTransaction::ActionType::Use : ItemUseInventoryTransaction::ActionType::Place;
+//
+//                //if (block.isFenceBlock() && LeadItem::canBindPlayerMobs) {
+//                if (block.isFenceBlock() && false) {
+//                    result.mResult = (int)InteractionResult::Result::SUCCESS | (int)InteractionResult::Result::SWING;
+//                }
+//                else {
+//                    // IMPORTANT WILL BREAK: GameMode::_sendUseItemOnEvents uses Player::getSelectedItem oof.
+//                    // IDA detects it doesn't actually use it tho?
+//                    result = self->useItemOn(selectedItemCopy, *pos, face, hitResult.mPos, nullptr);
+//                }
+//            }
+//
+//            // This section handles liquid clipped items, i.e. lillypads on water
+//            bool isLiquidClippingItem = (result.mResult & (int)InteractionResult::Result::SUCCESS) == 0
+//                && !selectedItemCopy.isNull()
+//                && selectedItemCopy.isLiquidClipItem()
+//                && !selectedItemCopy.shouldInteractionWithBlockBypassLiquid(block);
+//
+//            if (isLiquidClippingItem) {
+//                Log::Info("Todo: Handle liquid clipping items");
+//            }
+//
+//            // Block placement timings
+//            if ((result.mResult & (int)InteractionResult::Result::SUCCESS) != 0) {
+//				self->mBuildContext.mLastBuiltBlockPosition = calculatedPlacePos;
+//
+//                // Seems to get reset often enogu to be false each item use.
+//                if (!self->mBuildContext.mHasLastBuiltPosition) {
+//                    self->mMessenger->startSendItemUseOn(*pos, self->mBuildContext.mLastBuiltBlockPosition, (int)face);
+//					self->mBuildContext.mHasLastBuiltPosition = true;
+//                }
+//
+//				self->mLastBuildTime = std::chrono::steady_clock::now();
+//                if ((result.mResult & (int)InteractionResult::Result::SWING) != 0) {
+//					player.swing();
+//                } 
+//            }
+//
+//            PlayerEventCoordinator& eventCoordinator = player.getPlayerEventCoordinator();
+//
+//            std::function<EventResult(PlayerEventListener&)> ev = [&player, &itemInstance](PlayerEventListener& listener) {
+//                EventResult result = listener.onPlayerItemUseInteraction(player, itemInstance);
+//                return result;
+//            };
+//
+//            eventCoordinator.todoMoveToProperClass_processEvent(ev);
+//        }
+//    );
+//
+//    Log::Info("InteractResult: {}", result.mResult);
+//
+//    if (player.getLevel()->isClientSide()) {
+//		player.sendComplexInventoryTransaction(std::move(transaction));
+//    } 
+//     
+//    return true;
+//}
 
-    const ItemStack& selectedItem = inv->getItem(playerInv->mSelected);
-	const Item* item = selectedItem.getItem();
-
-    // The game seems to check things multiple times, i.e. selectedItem.isNull() covers some of the other cases in here
-    // but monkey see, monkey do, can always cleanup later
-    bool mayUseItem = !selectedItem.mValid
-        || !item
-        || selectedItem.isNull()
-        || selectedItem.mCount == 0
-        || !isSimTick
-        || item->canUseOnSimTick();
+bool tryUseItem(GameMode& self, const ItemStack& stack, const Player& player, Container& container, int slot, ContainerID containerId, const BlockPos& pos, FacingID face, bool isSimTick) {
+    const Item* item = stack.getItem();
+    bool mayUseItem = !stack.isNull() && (!isSimTick || (isSimTick && item->canUseOnSimTick()));
 
     if (!mayUseItem) {
-        Log::Info("May not use item!!");
+        Log::Info("May not use item: {}", stack);
         return false;
     }
 
-    self->mPlayerLastPosition = *player.getPosition();
+    self.mPlayerLastPosition = *player.getPosition();
     std::unique_ptr<ItemUseInventoryTransaction> transaction = std::make_unique<ItemUseInventoryTransaction>();
 
-	bool useLegacyTransaction = player.isClientSide() && player.mItemStackNetManager != nullptr;
-
-    // gsl::final_action runs code at the end of the scope
-    // Game only uses for legacy transaction
+    bool useLegacyTransaction = player.isClientSide() && player.mItemStackNetManager != nullptr;
     auto onScopeClosed = useLegacyTransaction
-        ? player.mItemStackNetManager->_tryBeginClientLegacyTransactionRequest()
-        : gsl::final_action<std::function<void()>>([]() {});
+       ? player.mItemStackNetManager->_tryBeginClientLegacyTransactionRequest()
+       : gsl::final_action<std::function<void()>>([]() {});
 
     InteractionResult result;
 
-    playerInv->createTransactionContext(
-        [&transaction, &self](Container& container, unsigned int slot, const ItemStack& oldStack, const ItemStack newStack) {
-            InventorySource source(InventorySourceType::ContainerInventory, ContainerID::CONTAINER_ID_INVENTORY, InventorySource::InventorySourceFlags::NoFlag);
+    container.createTransactionContext(
+        [&transaction, &self, containerId](Container& container, unsigned int slot, const ItemStack& oldStack, const ItemStack& newStack) {
+            InventorySource source(InventorySourceType::ContainerInventory, containerId, InventorySource::InventorySourceFlags::NoFlag);
             InventoryAction action(source, slot, oldStack, newStack);
-            
-			self->mPlayer.mTransactionManager.addExpectedAction(action);
+           
+			self.mPlayer.mTransactionManager.addExpectedAction(action);
 			transaction->mTransaction.addAction(action);
-        }, 
-        [&transaction, &self, pos, face, &result]() {
-            Player& player = self->mPlayer;
-            ILevel* level = player.mLevel;
+        },
+        [&transaction, &self, pos, face, &result, &stack, slot]() {
+            Player& player = self.mPlayer;
+            ILevel& level = *player.mLevel;
 
-			ItemStack selectedItemCopy = player.getSelectedItem();
-            ItemInstance itemInstance(selectedItemCopy);
+            ItemStack stackCopy = stack;
+            ItemInstance itemInstance(stackCopy);
+            NetworkItemStackDescriptor networkDescriptor(stackCopy);
 
-            NetworkItemStackDescriptor networkDescriptor(selectedItemCopy);
             transaction->mItem = networkDescriptor;
-            
-			HitResult hitResult = level->getHitResult();
-			HitResult liquidHit = level->getLiquidHitResult();
+            transaction->mSlot = slot;
 
-			const BlockSource& region = player.getDimensionBlockSourceConst();
-            const Block& block = region.getBlock(*pos);
+            HitResult hitResult = level.getHitResult();
+            HitResult liquidHit = level.getLiquidHitResult();
+
+            const BlockSource& region = player.getDimensionBlockSourceConst();
+            const Block& block = region.getBlock(pos);
 
             FacingID faceDir = face;
-            BlockPos calculatedPlacePos = self->_calculatePlacePos(selectedItemCopy, *pos, faceDir);
+            BlockPos calculatedPlacePos = self._calculatePlacePos(stackCopy, pos, faceDir);
 
-            if (hitResult.mType == HitResultType::TILE || liquidHit.mLiquid.y == 0 || self->mBuildContext.mHasBuildDirection) {
-                transaction->mPos = *pos;
-				transaction->setTargetBlock(block);
+            if (hitResult.mType == HitResultType::TILE || liquidHit.mLiquid.y == 0 || self.mBuildContext.mHasBuildDirection) {
+                transaction->mPos = pos;
+                transaction->setTargetBlock(block);
                 transaction->mFace = faceDir;
 
                 Vec3 clickPos;
@@ -116,10 +240,10 @@ bool GameMode_buildBlock(GameMode* self, BlockPos* pos, FacingID face, bool isSi
                     clickPos = Vec3::ZERO;
                 }
                 else {
-                    clickPos = hitResult.mPos - Vec3(*pos);
+                    clickPos = hitResult.mPos - Vec3(pos);
                 }
                 
-				transaction->mClickPos = clickPos;
+                transaction->mClickPos = clickPos;
                 transaction->mFromPos = *player.getPosition();
                 transaction->mActionType = face == FacingID::MAX ? ItemUseInventoryTransaction::ActionType::Use : ItemUseInventoryTransaction::ActionType::Place;
 
@@ -130,54 +254,69 @@ bool GameMode_buildBlock(GameMode* self, BlockPos* pos, FacingID face, bool isSi
                 else {
                     // IMPORTANT WILL BREAK: GameMode::_sendUseItemOnEvents uses Player::getSelectedItem oof.
                     // IDA detects it doesn't actually use it tho?
-                    result = self->useItemOn(selectedItemCopy, *pos, face, hitResult.mPos, nullptr);
+                    result = self.useItemOn(stackCopy, pos, face, hitResult.mPos, nullptr);
                 }
             }
 
             // This section handles liquid clipped items, i.e. lillypads on water
             bool isLiquidClippingItem = (result.mResult & (int)InteractionResult::Result::SUCCESS) == 0
-                && !selectedItemCopy.isNull()
-                && selectedItemCopy.isLiquidClipItem()
-                && !selectedItemCopy.shouldInteractionWithBlockBypassLiquid(block);
+                && !stackCopy.isNull()
+                && stackCopy.isLiquidClipItem()
+                && !stackCopy.shouldInteractionWithBlockBypassLiquid(block);
 
             if (isLiquidClippingItem) {
                 Log::Info("Todo: Handle liquid clipping items");
             }
 
-            // Block placement timings
-            if ((result.mResult & (int)InteractionResult::Result::SUCCESS) != 0) {
-				self->mBuildContext.mLastBuiltBlockPosition = calculatedPlacePos;
+           // Block placement timings
+           if ((result.mResult & (int)InteractionResult::Result::SUCCESS) != 0) {
+				self.mBuildContext.mLastBuiltBlockPosition = calculatedPlacePos;
 
                 // Seems to get reset often enogu to be false each item use.
-                if (!self->mBuildContext.mHasLastBuiltPosition) {
-                    self->mMessenger->startSendItemUseOn(*pos, self->mBuildContext.mLastBuiltBlockPosition, (int)face);
-					self->mBuildContext.mHasLastBuiltPosition = true;
+                if (!self.mBuildContext.mHasLastBuiltPosition) {
+                    self.mMessenger->startSendItemUseOn(pos, self.mBuildContext.mLastBuiltBlockPosition, (int)face);
+                        self.mBuildContext.mHasLastBuiltPosition = true;
                 }
 
-				self->mLastBuildTime = std::chrono::steady_clock::now();
+				self.mLastBuildTime = std::chrono::steady_clock::now();
                 if ((result.mResult & (int)InteractionResult::Result::SWING) != 0) {
-					player.swing();
+                        player.swing();
                 } 
-            }
-
-            PlayerEventCoordinator& eventCoordinator = player.getPlayerEventCoordinator();
-
-            std::function<EventResult(PlayerEventListener&)> ev = [&player, &itemInstance](PlayerEventListener& listener) {
-                EventResult result = listener.onPlayerItemUseInteraction(player, itemInstance);
-                return result;
-            };
-
-            eventCoordinator.todoMoveToProperClass_processEvent(ev);
+           }
         }
     );
 
     Log::Info("InteractResult: {}", result.mResult);
 
-    if (player.getLevel()->isClientSide()) {
-		player.sendComplexInventoryTransaction(std::move(transaction));
-    } 
-     
+    if (player.isClientSide()) {
+        player.sendComplexInventoryTransaction(std::move(transaction));
+    }
+ 
     return true;
+}
+
+// Modded version of GameMode::buildBlock that supports offhand items
+bool GameMode_buildBlock(GameMode* self, BlockPos* pos, FacingID face, bool isSimTick) {
+	Player& player = self->mPlayer;
+	ILevel& level = *player.mLevel;
+	PlayerInventory& playerInv = *player.playerInventory;
+	Inventory& inv = *playerInv.mInventory.get();
+
+	const ItemStack& mainHandItem = playerInv.getSelectedItem();
+    ActorEquipmentComponent* equipment = player.tryGetComponent<ActorEquipmentComponent>();
+	const ItemStack& offHandItem = equipment->mHand->mItems[1];
+
+    Log::Info("mainhand {}", mainHandItem);
+    // bool usedMainhand = tryUseItem(*self, mainHandItem, player, inv, playerInv.mSelected, ContainerID::CONTAINER_ID_INVENTORY, *pos, face, isSimTick);
+    bool usedMainhand = false;
+    bool usedOffhand = false;
+
+    if (!usedMainhand) {
+        Log::Info("offhand {}", offHandItem);
+        usedOffhand = tryUseItem(*self, offHandItem, player, *equipment->mHand, 1, ContainerID::CONTAINER_ID_OFFHAND, *pos, face, isSimTick);
+    }
+
+    return usedMainhand || usedOffhand;
 }
 
 
--- src/dllmain.hpp
+++ src/dllmain.hpp
@@ -1,10 +1,34 @@
 #pragma once
+#include <format>
 #include <Windows.h>
 #include <amethyst/runtime/ModContext.hpp>
 #include <amethyst/runtime/events/GameEvents.hpp> // OnStartJoinGameEvent
+#include <minecraft/src/common/world/item/ItemStack.hpp>
+#include <minecraft/src/common/world/item/Item.hpp>
 
 #define ModFunction extern "C" __declspec(dllexport)
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     return TRUE;
 }
+
+template <>
+struct std::formatter<ItemStack> {
+    constexpr auto parse(std::format_parse_context& ctx)
+    {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto format(const ItemStack& stack, FormatContext& ctx) const
+    {
+        if (stack.isNull()) {
+            return std::format_to(ctx.out(), "ItemStack(Null)");
+        }
+
+        return std::format_to(ctx.out(), "ItemStack(ID: {}, Count: {})",
+            stack.getItem()->mFullName.getString(),
+            stack.mCount
+        );
+    }
+};
