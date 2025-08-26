b4194b214e519268ef7b1ccc1400365192475dcd
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-08-26 18:02:34 +0100

    Offhand swap

--- src/features/SwapOffhandKey.hpp
+++ src/features/SwapOffhandKey.hpp
@@ -22,59 +22,73 @@ public:
 
     virtual void write(BinaryStream& stream) override {
         Log::Info("[SwapOffhandPacket::write]");
+		stream.writeString("SwapOffhand");
     }
 
     virtual Bedrock::Result<void, std::error_code> read(ReadOnlyBinaryStream& stream) override {
         Log::Info("[SwapOffhandPacket::read]");
+
+        if (stream.getString().value() != "SwapOffhand") {
+            Log::Info("SwapOffhandPacket: Invalid packet data");
+            return Bedrock::Result<void, std::error_code>();
+		}
+
         return Bedrock::Result<void, std::error_code>();
     }
 
     virtual Bedrock::Result<void, std::error_code> _read(ReadOnlyBinaryStream& stream) override {
+		Log::Info("[SwapOffhandPacket::_read]");
         return Bedrock::Result<void, std::error_code>();
+    }
+
+    virtual ~SwapOffhandPacket() override {
+		Packet::~Packet();
     }
 };
 
 template <>
 class PacketHandlerDispatcherInstance<SwapOffhandPacket, false> : public IPacketHandlerDispatcher {
 public:
-    virtual void handle(const NetworkIdentifier& networkId, NetEventCallback& netEvent, std::shared_ptr<Packet> _packet) const {
-  //      SwapOffhandPacket& packet = *(SwapOffhandPacket*)_packet.get();
-  //      ServerNetworkHandler& serverNetwork = (ServerNetworkHandler&)netEvent;
+    virtual void handle(const NetworkIdentifier& networkId, NetEventCallback& netEvent, std::shared_ptr<Packet>& packet) const override {
+		Log::Info("PacketHandlerDispatcherInstance<SwapOffhandPacket>::handle");
 
-  //      uintptr_t** address = *(uintptr_t***)&serverNetwork;
-		//
-  //      ServerPlayer* serverPlayer = serverNetwork._getServerPlayer(networkId, SubClientId::PrimaryClient);
+        //SwapOffhandPacket& packet = *(SwapOffhandPacket*)_packet.get();
+        ServerNetworkHandler& serverNetwork = (ServerNetworkHandler&)netEvent;
 
-  //      if (serverPlayer == nullptr) {
-  //          Log::Info("SwapOffhandPacket: ServerPlayer not found?");
-  //          return;
-		//}
+        uintptr_t** address = *(uintptr_t***)&serverNetwork;
+		
+        ServerPlayer* serverPlayer = serverNetwork._getServerPlayer(networkId, SubClientId::PrimaryClient);
 
-		//const PlayerInventory& inventory = serverPlayer->getSupplies();
-		//const ItemStack& mainhandStack = inventory.getSelectedItem();
-		//ItemStack mainhandCopy = mainhandStack;
+        if (serverPlayer == nullptr) {
+            Log::Info("SwapOffhandPacket: ServerPlayer not found?");
+            return;
+		}
 
-  //      ActorEquipmentComponent& equipment = *serverPlayer->tryGetComponent<ActorEquipmentComponent>();
-  //      const ItemStack& offhandStack = equipment.mHand->mItems[1];
-  //      ItemStack offhandCopy = offhandStack;
+		const PlayerInventory& inventory = serverPlayer->getSupplies();
+		const ItemStack& mainhandStack = inventory.getSelectedItem();
+		ItemStack mainhandCopy = mainhandStack;
 
-  //      inventory.mInventory->createTransactionContext(
-  //          [](Container& container, int slot, ItemStack const& from, ItemStack const& to) {
-  //              
-  //          },
-  //          [&inventory, offhandCopy]() {
-  //              inventory.mInventory->setItem(inventory.mSelected, offhandCopy);
-  //          }
-  //      );
+        ActorEquipmentComponent& equipment = *serverPlayer->tryGetComponent<ActorEquipmentComponent>();
+        const ItemStack& offhandStack = equipment.mHand->mItems[1];
+        ItemStack offhandCopy = offhandStack;
 
-  //      equipment.mHand->createTransactionContext(
-  //          [](Container& container, int slot, ItemStack const& from, ItemStack const& to) {
+        inventory.mInventory->createTransactionContext(
+            [](Container& container, int slot, ItemStack const& from, ItemStack const& to) {
+                
+            },
+            [&inventory, offhandCopy]() {
+                inventory.mInventory->setItem(inventory.mSelected, offhandCopy);
+            }
+        );
 
-  //          },
-  //          [&equipment, mainhandCopy]() {
-  //              equipment.mHand->setItem(1, mainhandCopy);
-  //          }
-  //      );
+        equipment.mHand->createTransactionContext(
+            [](Container& container, int slot, ItemStack const& from, ItemStack const& to) {
+
+            },
+            [&equipment, mainhandCopy]() {
+                equipment.mHand->setItem(1, mainhandCopy);
+            }
+        );
     }
 };
 
@@ -108,35 +122,35 @@ void RegisterKeyListener(RegisterInputsEvent& ev) {
 		ILevel& level = *player.getLevel();
         PacketSender& packetSender = *level.getPacketSender();
 
-        SwapOffhandPacket swapOffhandPacket;
-        packetSender.sendToServer(swapOffhandPacket);
+        SwapOffhandPacket* packet = new SwapOffhandPacket(); // intentionally leaked
+        packetSender.sendToServer(*packet);
 
 		// Swap items clientside for instant feedback
-  //      const PlayerInventory& inventory = player.getSupplies();
-  //      const ItemStack& mainhandStack = inventory.getSelectedItem();
-		//ItemStack mainhandCopy = mainhandStack;
+        const PlayerInventory& inventory = player.getSupplies();
+        const ItemStack& mainhandStack = inventory.getSelectedItem();
+		ItemStack mainhandCopy = mainhandStack;
 
-  //      const ActorEquipmentComponent& equipment = *player.tryGetComponent<ActorEquipmentComponent>();
-  //      const ItemStack& offhandStack = equipment.mHand->mItems[1];
-		//ItemStack offhandCopy = offhandStack;
+        const ActorEquipmentComponent& equipment = *player.tryGetComponent<ActorEquipmentComponent>();
+        const ItemStack& offhandStack = equipment.mHand->mItems[1];
+		ItemStack offhandCopy = offhandStack;
 
-  //      inventory.mInventory->createTransactionContext(
-  //          [](Container& container, int slot, ItemStack const& from, ItemStack const& to) {
+        inventory.mInventory->createTransactionContext(
+            [](Container& container, int slot, ItemStack const& from, ItemStack const& to) {
 
-  //          },
-  //          [&inventory, offhandCopy]() {
-  //              inventory.mInventory->setItem(inventory.mSelected, offhandCopy);
-  //          }
-  //      );
+            },
+            [&inventory, offhandCopy]() {
+                inventory.mInventory->setItem(inventory.mSelected, offhandCopy);
+            }
+        );
 
-  //      equipment.mHand->createTransactionContext(
-  //          [](Container& container, int slot, ItemStack const& from, ItemStack const& to) {
+        equipment.mHand->createTransactionContext(
+            [](Container& container, int slot, ItemStack const& from, ItemStack const& to) {
 
-  //          },
-  //          [&equipment, mainhandCopy]() {
-  //              equipment.mHand->setItem(1, mainhandCopy);
-  //          }
-  //      );
+            },
+            [&equipment, mainhandCopy]() {
+                equipment.mHand->setItem(1, mainhandCopy);
+            }
+        );
     });
 }
 
@@ -162,4 +176,16 @@ void RegisterSwapOffhandKey() {
 
     hooks.RegisterFunction<&allowIncomingPacketId>("48 89 5C 24 ? 48 89 6C 24 ? 56 57 41 56 48 83 EC ? 41 8B D8 48 8B F2");
 	hooks.CreateHook<&allowIncomingPacketId>(_allowIncomingPacketId, &allowIncomingPacketId);
+
+ //   hooks.RegisterFunction<&allocate>("B8 ? ? ? ? 48 3B D0 48 0F 42 D0 48 8B CA");
+	//hooks.CreateHook<&allocate>(_allocate, &allocate);
+
+	//hooks.RegisterFunction<&release>("48 8B CA 48 FF 25 ? ? ? ? CC CC CC CC CC CC 40 53 48 83 EC ? 48 8B D9");
+	//hooks.CreateHook<&release>(_release, &release);
+
+    int flags = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
+    flags |= _CRTDBG_ALLOC_MEM_DF |    // Keep track of all allocations
+        _CRTDBG_CHECK_ALWAYS_DF | // Check heap on every allocation/free
+        _CRTDBG_LEAK_CHECK_DF;    // Dump leaks at exit
+    _CrtSetDbgFlag(flags);
 }
