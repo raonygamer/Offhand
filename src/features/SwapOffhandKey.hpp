d63c96f9a07013cfa48aee3be8f601d2cbd074b1
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-08-26 02:07:02 +0100

    current

--- src/features/SwapOffhandKey.hpp
+++ src/features/SwapOffhandKey.hpp
@@ -8,6 +8,7 @@
 #include <minecraft/src/common/network/ServerNetworkHandler.hpp>
 #include <minecraft/src/common/server/ServerPlayer.hpp>
 #include <amethyst/Formatting.hpp>
+#include <minecraft/src-deps/input/MouseDevice.hpp>
 
 class SwapOffhandPacket : public Packet {
 public:
@@ -21,12 +22,10 @@ public:
 
     virtual void write(BinaryStream& stream) override {
         Log::Info("[SwapOffhandPacket::write]");
-        stream.writeUnsignedVarInt32(0); // game shits itself if the packet has nothing so add some shit
     }
 
     virtual Bedrock::Result<void, std::error_code> read(ReadOnlyBinaryStream& stream) override {
         Log::Info("[SwapOffhandPacket::read]");
-        stream.getUnsignedVarInt32();
         return Bedrock::Result<void, std::error_code>();
     }
 
@@ -39,43 +38,43 @@ template <>
 class PacketHandlerDispatcherInstance<SwapOffhandPacket, false> : public IPacketHandlerDispatcher {
 public:
     virtual void handle(const NetworkIdentifier& networkId, NetEventCallback& netEvent, std::shared_ptr<Packet> _packet) const {
-        SwapOffhandPacket& packet = *(SwapOffhandPacket*)_packet.get();
-        ServerNetworkHandler& serverNetwork = (ServerNetworkHandler&)netEvent;
+  //      SwapOffhandPacket& packet = *(SwapOffhandPacket*)_packet.get();
+  //      ServerNetworkHandler& serverNetwork = (ServerNetworkHandler&)netEvent;
 
-        uintptr_t** address = *(uintptr_t***)&serverNetwork;
-		
-        ServerPlayer* serverPlayer = serverNetwork._getServerPlayer(networkId, SubClientId::PrimaryClient);
+  //      uintptr_t** address = *(uintptr_t***)&serverNetwork;
+		//
+  //      ServerPlayer* serverPlayer = serverNetwork._getServerPlayer(networkId, SubClientId::PrimaryClient);
 
-        if (serverPlayer == nullptr) {
-            Log::Info("SwapOffhandPacket: ServerPlayer not found?");
-            return;
-		}
+  //      if (serverPlayer == nullptr) {
+  //          Log::Info("SwapOffhandPacket: ServerPlayer not found?");
+  //          return;
+		//}
 
-		const PlayerInventory& inventory = serverPlayer->getSupplies();
-		const ItemStack& mainhandStack = inventory.getSelectedItem();
-		ItemStack mainhandCopy = mainhandStack;
+		//const PlayerInventory& inventory = serverPlayer->getSupplies();
+		//const ItemStack& mainhandStack = inventory.getSelectedItem();
+		//ItemStack mainhandCopy = mainhandStack;
 
-        ActorEquipmentComponent& equipment = *serverPlayer->tryGetComponent<ActorEquipmentComponent>();
-        const ItemStack& offhandStack = equipment.mHand->mItems[1];
-        ItemStack offhandCopy = offhandStack;
+  //      ActorEquipmentComponent& equipment = *serverPlayer->tryGetComponent<ActorEquipmentComponent>();
+  //      const ItemStack& offhandStack = equipment.mHand->mItems[1];
+  //      ItemStack offhandCopy = offhandStack;
 
-        inventory.mInventory->createTransactionContext(
-            [](Container& container, int slot, ItemStack const& from, ItemStack const& to) {
-                
-            },
-            [&inventory, offhandCopy]() {
-                inventory.mInventory->setItem(inventory.mSelected, offhandCopy);
-            }
-        );
+  //      inventory.mInventory->createTransactionContext(
+  //          [](Container& container, int slot, ItemStack const& from, ItemStack const& to) {
+  //              
+  //          },
+  //          [&inventory, offhandCopy]() {
+  //              inventory.mInventory->setItem(inventory.mSelected, offhandCopy);
+  //          }
+  //      );
 
-        equipment.mHand->createTransactionContext(
-            [](Container& container, int slot, ItemStack const& from, ItemStack const& to) {
+  //      equipment.mHand->createTransactionContext(
+  //          [](Container& container, int slot, ItemStack const& from, ItemStack const& to) {
 
-            },
-            [&equipment, mainhandCopy]() {
-                equipment.mHand->setItem(1, mainhandCopy);
-            }
-        );
+  //          },
+  //          [&equipment, mainhandCopy]() {
+  //              equipment.mHand->setItem(1, mainhandCopy);
+  //          }
+  //      );
     }
 };
 
@@ -88,12 +87,12 @@ std::shared_ptr<Packet> createPacket(MinecraftPacketIds id) {
 
     // Vanilla packets.
     if (id < MinecraftPacketIds::EndId) {
-        return _createPacket.call<std::shared_ptr<Packet>>(id);
+		std::shared_ptr<Packet> packet = _createPacket.call<std::shared_ptr<Packet>>(id);
+		return packet;
     }
 
     // Custom packets
     if ((int)id == (int)MinecraftPacketIds::EndId + 1) {
-        Log::Info("createPacket made SwapOffhandPacket");
         auto shared = std::make_shared<SwapOffhandPacket>();
         shared->mHandler = &swapOffhandPacketHandler;
         return shared;
@@ -105,43 +104,51 @@ void RegisterKeyListener(RegisterInputsEvent& ev) {
 	Amethyst::InputAction& swapOffhandKey = inputManager.RegisterNewInput("swap_input", { 'F' }, true, Amethyst::KeybindContext::Gameplay);
 
     swapOffhandKey.addButtonDownHandler([](FocusImpact focus, IClientInstance& client) {
-        return; 
-
         LocalPlayer& player = *client.getLocalPlayer();
 		ILevel& level = *player.getLevel();
         PacketSender& packetSender = *level.getPacketSender();
 
         SwapOffhandPacket swapOffhandPacket;
         packetSender.sendToServer(swapOffhandPacket);
-		Log::Info("Sent SwapOffhandPacket to server");
 
 		// Swap items clientside for instant feedback
-        const PlayerInventory& inventory = player.getSupplies();
-        const ItemStack& mainhandStack = inventory.getSelectedItem();
-		ItemStack mainhandCopy = mainhandStack;
+  //      const PlayerInventory& inventory = player.getSupplies();
+  //      const ItemStack& mainhandStack = inventory.getSelectedItem();
+		//ItemStack mainhandCopy = mainhandStack;
 
-        const ActorEquipmentComponent& equipment = *player.tryGetComponent<ActorEquipmentComponent>();
-        const ItemStack& offhandStack = equipment.mHand->mItems[1];
-		ItemStack offhandCopy = offhandStack;
+  //      const ActorEquipmentComponent& equipment = *player.tryGetComponent<ActorEquipmentComponent>();
+  //      const ItemStack& offhandStack = equipment.mHand->mItems[1];
+		//ItemStack offhandCopy = offhandStack;
 
-        inventory.mInventory->createTransactionContext(
-            [](Container& container, int slot, ItemStack const& from, ItemStack const& to) {
+  //      inventory.mInventory->createTransactionContext(
+  //          [](Container& container, int slot, ItemStack const& from, ItemStack const& to) {
 
-            },
-            [&inventory, offhandCopy]() {
-                inventory.mInventory->setItem(inventory.mSelected, offhandCopy);
-            }
-        );
+  //          },
+  //          [&inventory, offhandCopy]() {
+  //              inventory.mInventory->setItem(inventory.mSelected, offhandCopy);
+  //          }
+  //      );
 
-        equipment.mHand->createTransactionContext(
-            [](Container& container, int slot, ItemStack const& from, ItemStack const& to) {
+  //      equipment.mHand->createTransactionContext(
+  //          [](Container& container, int slot, ItemStack const& from, ItemStack const& to) {
 
-            },
-            [&equipment, mainhandCopy]() {
-                equipment.mHand->setItem(1, mainhandCopy);
-            }
-        );
+  //          },
+  //          [&equipment, mainhandCopy]() {
+  //              equipment.mHand->setItem(1, mainhandCopy);
+  //          }
+  //      );
     });
+}
+
+SafetyHookInline _allowIncomingPacketId;
+
+bool allowIncomingPacketId(ServerNetworkHandler* self, const NetworkIdentifier& networkId, MinecraftPacketIds packet) {
+    if ((int)packet == (int)MinecraftPacketIds::EndId + 1) {
+		Log::Info("allowIncomingPacketId: SwapOffhandPacket");
+        return true;
+	}
+
+	return _allowIncomingPacketId.call<bool, ServerNetworkHandler*, const NetworkIdentifier&, MinecraftPacketIds>(self, networkId, packet);
 }
 
 void RegisterSwapOffhandKey() {
@@ -153,15 +160,6 @@ void RegisterSwapOffhandKey() {
     hooks.RegisterFunction<&MinecraftPackets::createPacket>("40 53 48 83 EC ? 45 33 C0 48 8B D9 FF CA 81 FA");
     hooks.CreateHook<&MinecraftPackets::createPacket>(_createPacket, &createPacket);
 
-	Amethyst::PatchManager& patches = Amethyst::GetPatchManager();
-
-    // Need to patch the max packet ID check in ServerNetworkHandler::allowIncomingPacketId
-    // original instruction - cmp r8d, 136h
-    uintptr_t maxPacketCheck = GetMinecraftBaseAddress() + 0x1737C2B;
-    uint8_t patch[7] = { 
-        0x41, 0x81, 0xF8, // cmp r8d
-        0x38, 0x01, 0, 0 // new max (312 in hex (0x0138), then reversed order)
-    };
-
-    patches.ApplyPatch(maxPacketCheck, patch, sizeof(patch));
+    hooks.RegisterFunction<&allowIncomingPacketId>("48 89 5C 24 ? 48 89 6C 24 ? 56 57 41 56 48 83 EC ? 41 8B D8 48 8B F2");
+	hooks.CreateHook<&allowIncomingPacketId>(_allowIncomingPacketId, &allowIncomingPacketId);
 }
